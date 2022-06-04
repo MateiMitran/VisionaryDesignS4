4dfdb1eb7e61b855087872f6145df271607ecf6b
Bianca Onea <biancaonea@Biancas-MacBook-Pro.local>
2022-06-04 12:51:07 +0200

    Fixed errors

--- VisionaryDesignApp/VisionaryDesignApp/MainScreen.swift
+++ VisionaryDesignApp/VisionaryDesignApp/MainScreen.swift
@@ -10,7 +10,55 @@ import SwiftUI
 struct MainScreen: View {
     
     var body : some View {
-        Text("Hei")
+        HStack
+        {
+            Button(action: {
+                print("Delete button tapped!")
+            }) {
+                Text("Furniture")
+                    .padding()
+                    .background(Color.purple)
+                    .foregroundColor(.white)
+                    .font(.caption)
+                    .frame(width: 150, height: 50)
+            }
+            
+            Button(action: {
+                print("Delete button tapped!")
+            }) {
+                Text("Seats & Chairs")
+                    .padding()
+                    .background(Color.purple)
+                    .foregroundColor(.white)
+                    .font(.caption)
+                    .frame(width: 150, height: 50)
+            }
+            
+            Button(action: {
+                print("Delete button tapped!")
+            }) {
+                Text("Tables")
+                    .padding()
+                    .background(Color.purple)
+                    .foregroundColor(.white)
+                    .font(.caption)
+                    .frame(width: 150, height: 50)
+            }
+            
+            Button(action: {
+                print("Delete button tapped!")
+            }) {
+                Text("Beds")
+                    .padding()
+                    .background(Color.purple)
+                    .foregroundColor(.white)
+                    .font(.caption)
+                    .frame(width: 120, height: 50)
+            }
+
+        }
+        .rotationEffect(.degrees(90))
+        .position(x: 270, y: 260)
     }
 }
 
