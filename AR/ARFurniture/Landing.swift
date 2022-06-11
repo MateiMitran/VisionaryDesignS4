46c70178e78511e0327d475238be746c6a7f3c73
Bianca Onea <biancaonea@Biancas-MacBook-Pro.local>
2022-06-11 12:07:00 +0200

    Changed landing page and added app icon

--- AR/ARFurniture/Landing.swift
+++ AR/ARFurniture/Landing.swift
@@ -9,49 +9,55 @@ import Foundation
 import SwiftUI
 
 struct Landing: View {
-    
-    var body : some View {
-        NavigationView {
-            GeometryReader { geo in
-                ZStack{
-                    Image("login")
-                        .resizable()
-                        .scaledToFill()
-                        .edgesIgnoringSafeArea(.all)
-                        .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
-                        .opacity(0.8)
+
+    var body: some View {
+        NavigationView{
+        VStack {
+            Image("img1")
+                .resizable()
+                .frame(width: 200, height: 550)
+                .position(x: 85, y: 110)
+            
+            Image("img2")
+                .resizable()
+                .frame(width: 230, height: 220)
+                .position(x: 320, y: -110)
+            
+            Image("img3")
+                .resizable()
+                .frame(width: 230, height: 220)
+                .position(x: 320, y: -10)
+            
+            
+            Text("Visionary")
+                .font(.custom("Arsenal-Regular", size: 60))
+                .position(x: 200, y: 55)
+                .foregroundColor(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.72, saturation: 0.466, brightness: 0.648)/*@END_MENU_TOKEN@*/)
+            
+            Text("Design")
+                .font(.custom("PlayfairDisplay-Regular", size: 45))
+                .position(x: 200, y: -30)
+                .foregroundColor(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.72, saturation: 0.466, brightness: 0.648)/*@END_MENU_TOKEN@*/)
+            
+            NavigationLink(destination: ContentView(), label: {Text("Get Started")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .cornerRadius(30)
                     
-                    VStack(spacing:150) {
-                        Image("logoapp")
-                            .padding()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30)
-                            .padding(.bottom)
-                            .position(x: 200, y: 280)
-                        
-                        NavigationLink(destination: ContentView(), label: {Text("Get Started")
-                                .bold()
-                                .frame(width: 280, height: 50)
-                                .background(Color.purple)
-                                .foregroundColor(.white)
-                                .cornerRadius(30)
-                        })
-                        .navigationBarBackButtonHidden(true)
-                        .navigationBarHidden(true)
-                        .navigationTitle("")
-                        .position(x: 200, y: 250)
-//                        .frame(maxHeight: .infinity, alignment: .bottom)
-//                        .frame(width: 280, height: 50, alignment: .bottom)
-                        
-                    }
-//                    .frame(maxHeight: .infinity, alignment: .bottom)
-                }
-            }
+            })
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+            .navigationTitle("")
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.72, saturation: 0.466, brightness: 0.648)/*@END_MENU_TOKEN@*/)
+            .cornerRadius(30)
         }
+
+
+    
+    }
     }
 }
-
-
 
 struct Landing_Previews: PreviewProvider {
     static var previews: some View {
